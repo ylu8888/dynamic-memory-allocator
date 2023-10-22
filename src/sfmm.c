d26b0e5b48950425fea4b4eb9c6a5857e0a8b309
ylu <123523291+ylu8888@users.noreply.github.com>
2023-10-22 00:22:17 -0400

    notes

--- src/sfmm.c
+++ src/sfmm.c
@@ -9,7 +9,26 @@
 #include "sfmm.h"
 
 void *sf_malloc(size_t size) {
-    // To be implemented.
+    if (size == 0) {
+        return NULL;
+    }
+    //if allocation not successful
+    // sf_errno = ENOMEM;
+    // return NULL;
+
+    sf_mem_grow(); //call mem grow
+
+    
+
+    //call mem grow
+    //remember the first 8 bytes of heap are empty, unused
+    //skip to 8 bytes after that the heap and then start doing prologue, epilouge
+    //put a prologue in front of heap which is block of minimum size, has the header 16 bytes of padding and a footer
+    //put a epilogue which is an 8 byte header whcih doesnt have footer or padding
+    //EVERY THING in between epilogue and prologue those 4800 bytes you have to make into another block
+    // that will be your first free block, you put a header for that too and a footer for that
+   
+    
     abort();
 }
 
